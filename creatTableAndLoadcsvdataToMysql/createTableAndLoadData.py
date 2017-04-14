74e13f7a642fc3b3cab6d95a0c98859c263f3abb
xiehl <123872842@qq.com>
2017-04-14 19:00:09 +0800

    实现程序从文件中自动载入数据到数据库

--- creatTableAndLoadcsvdataToMysql/createTableAndLoadData.py
+++ creatTableAndLoadcsvdataToMysql/createTableAndLoadData.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import MySQLdb
 type_dic = {'int':'int', 'object':'varchar(100)', 'datetime':'datetime', 'float':'float'}
 
 def test_read_csv():
@@ -11,6 +12,7 @@ def test_read_csv():
 
 def get_create_table_sql_from_csvfile(path, table_name, _field_types):
     text = pd.read_csv(path)
+    path +='1';
     columns = text.columns
     dtypes = text.dtypes
     # print type(columns[0])
@@ -24,14 +26,16 @@ def get_create_table_sql_from_csvfile(path, table_name, _field_types):
     sql = sql[:-1]
     sql += ');'
     print sql
+    text.to_csv(path, index=False, header=False)
     return sql
 
 
 def generate_loadfile_sql(_infile, _table_name):
-    sql = 'load data local infile ' +"'"+_infile+"' into table " + _table_name
+    sql = 'load data local infile ' +"'"+_infile+"1"+"' into table " + _table_name
     sql += " COLUMNS TERMINATED BY ',' "
     sql += "LINES TERMINATED BY '\\n';"
     print sql
+    return sql
 
 
 def test():
@@ -50,9 +54,43 @@ def generate_type_list(_pd_data, _datetime_indices=None):
     if _datetime_indices!=None:
         type_list[_datetime_indices] = 'datetime'
     print type_list
+    return type_list
 
 def test2():
     text = pd.read_csv("test.csv")
     generate_type_list(text, 3)
+
+def generate_sql(_infile, _tableName, _datetime_index=None):
+    data = pd.read_csv(_infile)
+    typeList = generate_type_list(data, _datetime_index)
+    createTableSql = get_create_table_sql_from_csvfile(_infile, _tableName, typeList)
+    loadFileSql = generate_loadfile_sql(_infile, _tableName)
+    return createTableSql, loadFileSql
+
+def get_connect(_dbName,  _user, _passwd, _host='localhost'):
+    try:
+        t = MySQLdb.connect(db= _dbName, host=_host, user=_user, passwd=_passwd)
+        return t
+    except Exception as e:
+        print e
+
+
+def execute_sql(_con, _sql):
+    cursor = _con.cursor()
+    cursor.execute(_sql)
+    cursor.close()
+    _con.commit()
+
+
+def test3():
+    createSql, loadSql = generate_sql("test.csv", 'train', 3)
+    con = get_connect("test", "root", "password")
+    print loadSql
+    execute_sql(con, createSql)
+    execute_sql(con, loadSql)
+
+
 if __name__ == "__main__":
-    test2()
+    # generate_sql("test.csv", 'train', 3)
+    # get_connect()
+    test3()
