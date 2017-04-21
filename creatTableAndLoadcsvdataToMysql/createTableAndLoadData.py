b4bdc80431d7aa2ecf56e15796cb49263d2d1a3f
xiehl <123872842@qq.com>
2017-04-21 17:23:46 +0800

    本地生成表格

--- creatTableAndLoadcsvdataToMysql/createTableAndLoadData.py
+++ creatTableAndLoadcsvdataToMysql/createTableAndLoadData.py
@@ -5,19 +5,23 @@ import MySQLdb
 type_dic = {'int':'int', 'object':'varchar(100)', 'datetime':'datetime', 'float':'float'}
 
 def test_read_csv():
-    text = pd.read_csv("test.csv")
+    text = pd.read_csv("dump.csv", header=None)
     print text.columns
     print text.dtypes
 
 
 
 def get_create_table_sql_from_csvfile(path, table_name, _field_types):
+    '''
+    从要输入数据库的文件形式构造出建表语句，以及一个去掉表头的文件
+    :param path: 要输入的文件路径
+    :param table_name: 要建立的表的名称
+    :param _field_types: 数据库中每列的类型组成的字典
+    :return: 一个建表的sql语句
+    '''
     text = pd.read_csv(path)
     path +='1';
     columns = text.columns
-    dtypes = text.dtypes
-    # print type(columns[0])
-    # print type(dtypes[columns[0]])
     i = 0
     sql = "create table "+table_name+"("
     for header in columns:
@@ -27,11 +31,18 @@ def get_create_table_sql_from_csvfile(path, table_name, _field_types):
     sql = sql[:-1]
     sql += ');'
     print sql
+
     text.to_csv(path, index=False, header=False)
     return sql
 
 
 def generate_loadfile_sql(_infile, _table_name):
+    '''
+    生成载入数据到数据库的sql语句
+    :param _infile: 与get_create_table_sql_from_csvfile函数中的path一样
+    :param _table_name: 与get_create_table_sql_from_csvfile函数中的table_name要一致
+    :return: 一个载入数据到数据库语句
+    '''
     sql = 'load data local infile ' +"'"+_infile+"1"+"' into table " + _table_name
     sql += " COLUMNS TERMINATED BY ',' "
     sql += "LINES TERMINATED BY '\\n';"
@@ -81,6 +92,19 @@ def generate_select_sql_by_range_date(_begin_date, _end_date, _table_name, _colu
     return sql
 
 
+def generate_select_sql_dumpfile(_path, _tableName):
+    '''
+    该sql语句只能导出到服务器，不能导出到本地
+    :param _path:
+    :param _tableName:
+    :return:
+    '''
+    sql = 'select * into local outfile \'' +_path +'\' '
+    sql += " FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' "
+    sql += " LINES TERMINATED BY '\\n' "
+    sql += ' from ' + _tableName +";"
+    print sql
+    return sql
 
 def execute_sql(_con, _sql):
     cursor = _con.cursor()
@@ -97,12 +121,30 @@ def execute_query(_con, _sql):
         for r in row:
             print r
 
+
+def dump_file(_con, _sql, _path):
+    cursor = _con.cursor()
+    cursor.execute(_sql)
+    table =''
+    for row in cursor.fetchall():
+        line = ''
+        for r in row:
+            line += str(r)+","
+        line = line[:-1] +'\n'
+        table += line
+
+    f = open(_path, 'wb')
+    f.write(table)
+    f.close()
+    cursor.close()
+
+
 def test3():
     createSql, loadSql = generate_sql("test.csv", 'train', 3)
     print createSql
     print loadSql
     # con = get_connect("test", "root", "password")
-    con = get_connect("xie", "root", "abc123", "139.199.32.123")
+    con = get_connect("xie", "root", "password", "139.199.32.123")
     print loadSql
     execute_sql(con, 'set global max_allowed_packet=67108864')  #用来改变max_allowed_packet
     #解决报错 'Lost connection to MySQL server during query'
@@ -111,12 +153,18 @@ def test3():
 
 def test4():
     sql = generate_select_sql_by_range_date('2012-12-02', '2013-02-02', 'train', 'header4')
-    con = get_connect("xie", "root", "abc123", "139.199.32.123")
+    con = get_connect("xie", "root", "password", "139.199.32.123")
     execute_query(con, sql)
+    dump_file(con, sql, "dump.csv")
 
+def test5():
+    sql = generate_select_sql_dumpfile("dump.csv", "train")
+    con = get_connect("xie", "root", "password", "139.199.32.123")
+    execute_query(con, sql)
 
 if __name__ == "__main__":
     # generate_sql("test.csv", 'train', 3)
     # get_connect()
     # test3()
-    test4()
+    # test4()
+    test_read_csv()
